2cac063c6b049b64fa9f1412c517056d0a44414c
nikaxen <nikaxen96@gmail.com>
2017-10-29 17:37:54 +0300

    2 commit

--- app/src/main/java/com/example/nik_ax/androidlab5db/MainActivity.java
+++ app/src/main/java/com/example/nik_ax/androidlab5db/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.nik_ax.androidlab5db;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,6 +20,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     Button btnAdd, btnRead, btnClear;
     EditText etName, etEmail;
     DBHelper dbHelper;
+
+    public void gotoAct2(View view){
+        Intent intent = new Intent(this, ActivityOut.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +63,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 cv.put("name", name);
                 cv.put("email", email);
 
-                long rowID = db.insert("mytable", null, cv);
+                long rowID = db.insert("mytable1", null, cv);
                 Log.d(LOG_TAG, "row inserted, ID = " + rowID);
                 break;
             case R.id.btnRead:
                 Log.d(LOG_TAG, "--- Rows in mytable ---");
 
-                Cursor c = db.query("mytable", null, null, null, null, null, null);
+                Cursor c = db.query("mytable1", null, null, null, null, null, null);
 
                 if (c.moveToFirst()) {
                     int idColIndex = c.getColumnIndex("id");
@@ -81,13 +87,19 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 break;
             case R.id.btnClear:
                 Log.d(LOG_TAG, "--- Clear mytable ---");
-                int clearCount = db.delete("mytable", null, null);
+                int clearCount = db.delete("mytable1", null, null);
                 Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+            case R.id.btnRedirect:
+                Intent intent = new Intent(this, ActivityOut.class);
+                startActivity(intent);
                 break;
         }
         dbHelper.close();
     }
-    class DBHelper extends SQLiteOpenHelper {
+    static class DBHelper extends SQLiteOpenHelper {
+        private String LOG_TAG;
+
         public DBHelper(Context context) {
             super(context, "myDB", null, 1);
         }
@@ -95,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         @Override
         public void onCreate(SQLiteDatabase db){
             Log.d(LOG_TAG, "--- OnCreate database ---");
-            db.execSQL("create table mytable (id integer primary key autoincrement,"
+            db.execSQL("create table mytable1 (_id integer primary key autoincrement,"
             + "name text,"
             + "email text" + ");");
         }
